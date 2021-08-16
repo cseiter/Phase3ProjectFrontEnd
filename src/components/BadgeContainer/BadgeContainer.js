c8fd0ddb192871bf63bc84cd602574589cf632c8
chris <cseiter@gmail.com>
2021-08-15 20:53:05 -0400

    add test deleteBadge function

--- src/components/BadgeContainer/BadgeContainer.js
+++ src/components/BadgeContainer/BadgeContainer.js
@@ -2,6 +2,10 @@ import React,{ useState, useEffect } from 'react';
 import Card from 'react-bootstrap/esm/Card';
 import Button from 'react-bootstrap/Button';
 
+function deleteBadge () {
+    alert("deleting badge!");
+}
+
 export default function ScoutContainer() {
     useEffect(() => {
         fetch('http://localhost:9393/badges')
@@ -14,7 +18,7 @@ export default function ScoutContainer() {
     const badgesList = badges.map(badge =>
     <Card style={{ width: '25rem'}}>
         <Card.Body>
-            <Card.Title>{badge.badges_name}<Button variant="danger">X</Button></Card.Title>
+            <Card.Title>{badge.badges_name}<Button variant="danger" onClick={deleteBadge}>X</Button></Card.Title>
             <Card.Header>Is this required for Eagle?</Card.Header>
             <Card.Text>{badge.badges_is_eagle}</Card.Text>
             <Card.Header>Is this earned?</Card.Header>
